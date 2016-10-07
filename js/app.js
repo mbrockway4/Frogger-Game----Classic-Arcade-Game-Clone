847d1115089454e9d7f30f662d704ad6bc520cf9
mbrockway4 <mbrockway4@gmail.com>
2016-10-07 16:11:40 -0400

    version2
    fixed issues

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // Enemies our player must avoid
 var Enemy = function(xPos, yPos) {
     var speed = Math.floor(Math.random() * 500 + 100);
-    this.x = xPos
-    this.y = yPos
+    this.x = xPos;
+    this.y = yPos;
     this.delta = speed;
     this.w = [xPos - 50, xPos + 50];
     this.h = [yPos + 100, yPos - 100];
@@ -21,8 +21,23 @@ Enemy.prototype.update = function(dt, x, y) {
         this.x = 0;
         this.delta = Math.floor(Math.random() * 500 + 100);
     }
-    Dead(this);
+    this.Dead();
 };
+
+//Function determines if player has collided with an Enemy object resulting in loss of game and reset of player
+Enemy.prototype.Dead = function() {
+        if (((player.x >= this.x - 50) && (player.x <= this.x + 50)) && ((player.y >= this.y - 40) && (player.y <= this.y + 40))) {
+            ctx.clearRect(0, 0, 300, 300);
+            console.log("collision ", player.x, player.y);
+                //debugger;
+            deadcount = deadcount + 1;
+            ctx.font = "30px Arial";
+            ctx.fillText("You Lose! - " + deadcount, 10, 50);
+            console.log("Deaths ", deadcount);
+            player.x = 202.5;
+            player.y = 375;
+        }
+    };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -54,63 +69,51 @@ Player.prototype.update = function(dt) {
 };
 Player.prototype.handleInput = function(keyPress) {
     if (keyPress == 'left') {
-        player.x = player.x - player.delta;
+        this.x = this.x - this.delta;
     }
     if (keyPress == 'right') {
-        player.x = player.x + this.delta;
+        this.x = this.x + this.delta;
     }
     if (keyPress == 'up') {
-        player.y = player.y - this.delta;
+        this.y = this.y - this.delta;
     }
     if (keyPress == 'down') {
-        player.y = player.y + this.delta;
+        this.y = this.y + this.delta;
     }
-    checkBounds()
-    player.update(player.x, player.y)
-}
+    this.checkBounds();
+    this.update(this.x, this.y);
+};
 
 //Function to ensure player does not go out of bounds.  Also determines if game is won.
-var checkBounds = function() {
-    if (player.y >= 400) {
-        player.y = 375;
+Player.prototype.checkBounds = function() {
+    if (this.y >= 400) {
+        this.y = 375;
     }
-    if (player.y <= 0) {
-        player.y = -25;
-        console.log("Win ", wincount)
+    if (this.y <= 0) {
+        this.y = -25;   
         wincount = wincount + 1;
+        console.log("Win ", wincount);
         ctx.clearRect(0, 0, 200, 200);
         ctx.font = "30px Arial";
         ctx.fillText("You Win! - " + wincount, 10, 50);
-        player = new Player(202.5, 375)
+        //reset game
+        this.x = 202.5;
+        this.y = 375;
     }
-    if (player.x >= 400) {
-        player.x = 405;
+    if (this.x >= 400) {
+        this.x = 405;
     }
-    if (player.x <= 0) {
-        player.x = 1;
+    if (this.x <= 0) {
+        this.x = 1;
     }
-}
-
-//Function determines if player has collided with an Enemy object resulting in loss of game and reset of player
-var Dead = function(enemy) {
-        if (((player.x >= enemy.x - 50) && (player.x <= enemy.x + 50)) && ((player.y >= enemy.y - 40) && (player.y <= enemy.y + 40))) {
-            ctx.clearRect(0, 0, 300, 300);
-            console.log("collision ", player.x, player.y)
-                //debugger;
-            deadcount = deadcount + 1;
-            ctx.font = "30px Arial";
-            ctx.fillText("You Lose! - " + deadcount, 10, 50);
-            console.log("Deaths ", deadcount)
-            player = new Player(202.5, 375);
-        }
-    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var speed = Math.floor(Math.random() * 400 + 300);
-var allEnemies = [new Enemy(0, 140), new Enemy(0, 55)]
-var player = new Player(202.5, 400)
+var allEnemies = [new Enemy(0, 140), new Enemy(0, 55)];
+var player = new Player(202.5, 400);
 var wincount = 0;
 var deadcount = 0;
 // Draw the Player on the screen, required method for game
